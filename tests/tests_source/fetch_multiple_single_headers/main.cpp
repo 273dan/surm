333ace76bc1a884670f8484f925d56ca0b7b1e6d
Dan Kirby <dankirby273@gmail.com>
2026-03-08 20:45:45 +0000

    test: added multiple single-header-file dependency test

---
+++ tests/tests_source/fetch_multiple_single_headers/main.cpp
@@ -0,0 +1,8 @@
+#include <CLIUtils/CLI11/CLI11.hpp>
+#include <nlohmann/json/json.hpp>
+#include <Neargye/nameof/nameof.hpp>
+#include <iostream>
+
+int main() {
+  std::cout << "This code compiled!\n";
+}
